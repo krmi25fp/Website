55571a3f907d48fe52916961720c94a5c78dfd10
krmi25fp <krmi25fp@student.ju.se>
2026-03-12 11:56:39 +0100

    Display cart items using localStorage

--- script.js
+++ script.js
@@ -19,3 +19,24 @@ addToCartButtons.forEach((button) => {
     alert(name + " added to cart!");
   });
 });
+
+const cartItemsContainer = document.getElementById("cart-items");
+
+if (cartItemsContainer) {
+  const cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+  if (cart.length === 0) {
+    cartItemsContainer.innerHTML = "<p>Your cart is empty.</p>";
+  } else {
+    cart.forEach((item) => {
+      const div = document.createElement("div");
+
+      div.innerHTML = `
+      <h2>${item.name}</h2>
+      <p>${item.price} kr</p>
+    `;
+
+      cartItemsContainer.appendChild(div);
+    });
+  }
+}
